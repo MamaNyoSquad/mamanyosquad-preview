f0ab87173aa3e9ff792c8cf777aa586569ae9251
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-06-03 13:20:55 +0000

    v20240518.3.1-20240503

---
+++ js/banner.js
@@ -0,0 +1,11 @@
+function startBanner() {
+
+  var bnrStyle = document.getElementById("specialBanner").style;
+  var bnrHTML = document.getElementById("specialBanner");
+
+  bnrStyle.display = "none";
+  bnrStyle.fontFamily = "'Poppins', 'Noto Serif JP', sans-serif !important";
+
+  bnrHTML.innerHTML = `adasdsad`;
+
+}
---
+++ js/home.js
@@ -0,0 +1,3 @@
+function showVersion() {
+  document.getElementById("siteVersion").innerHTML = "v20240518.3.1-20240503";
+}
