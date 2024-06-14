87854a3787c8adfb6686768cf1b860d2bc836eda
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2024-06-14 06:14:32 +0000

    v20240518.4.23-20240614_preview

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // global init
 function init() {
+  previewRibbon(); // remove if migrating to prod
   triggerResponsive();
   showVersion();
   venusIslandTimeInit();
@@ -9,7 +10,7 @@ function init() {
 }
 
 function showVersion() {
-  document.getElementById("siteVersion").innerHTML = "v20240518.4.9-20240612_preview";
+  document.getElementById("siteVersion").innerHTML = "v20240518.4.23-20240614_preview";
 }
 
 // js clock code adapted from https://jsfiddle.net/mplungjan/mQrJn 
@@ -35,4 +36,18 @@ function venusIslandTimeInit() {
 }
 function venusIslandTimeClr() {
   clearTimeout(VIT);
+}
+
+function previewRibbon() {
+
+  var bannerPrv = document.createElement("link");
+  bannerPrv.type = "text/css"
+  bannerPrv.rel = "stylesheet";
+  bannerPrv.href = "https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css";
+
+  document.head.appendChild(bannerPrv);
+
+  const previewBanner = `<a id="bannerClr" class="github-fork-ribbon left-top" rel="noopener noreferrer" href="https://github.com/MamaNyoSquad/mamanyosquad-preview" target="_blank" data-ribbon="Preview Release">Preview Release</a>`;
+  document.body.insertAdjacentHTML("afterbegin", previewBanner);
+
 }
